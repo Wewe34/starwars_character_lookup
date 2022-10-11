ab0bce3385228de321c6a880c363bfb410028464
Cassandra Terry <cassandraterry@Cassandras-MacBook-Pro.local>
2022-10-11 02:33:31 -0500

    character route created to get character info, info displayed in view

--- client/src/Components/Profile.tsx
+++ client/src/Components/Profile.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-interface iCharacters {
+interface ICharacters {
     name: string, 
     height: string, 
     mass: string, 
@@ -11,47 +11,109 @@ interface iCharacters {
     birth_year: string, 
     gender: string, 
     homeworld: string, 
-    films: string[], 
+    films: IFilm[], 
     species: string[], 
-    vehicles: string[]
-    starships: string[]
+    vehicles: string[],
+    starships: IStarship[],
+    created: string, 
+    edited: string, 
+    url: string
+}
+
+interface IFilm {
+    title: string,
+    episode_id: number, 
+    opening_crawl: string, 
+    director: string, 
+    producer: string, 
+    release_date: string, 
+    characters: string[], 
+    planets: string[], 
+    starships: string[], 
+    vehicles:string[], 
+    species: string[], 
+    created:string, 
+    edited:string, 
+    url: string
+}
+
+interface IStarship {
+    name: string, 
+    model: string, 
+    manufacturer: string, 
+    cost_in_credits: string, 
+    length: string, 
+    max_atmosphering_speed: string, 
+    crew: string, 
+    passengers: string, 
+    cargo_capacity: string, 
+    consumables: string, 
+    hyperdrive_rating: string,
+    MGLT: string, 
+    starship_class: string, 
+    pilots: string[], 
+    films: string [], 
     created: string, 
     edited: string, 
     url: string
 }
 
 function Profile() {
-  const [characters, setCharacters] = useState<iCharacters[]>([]);
-  const [characterName, setCharacterName] = useState('');
+  const [characters, setCharacters] = useState<ICharacters[]>([]);
+  const [characterName, setCharacterName] = useState<String>('');
+  const [buttonToggle, setButtonToggle] = useState<Boolean>(false);
   
-  useEffect(() => {
-    axios.get("/characters", {params: {name: characterName}}).then(result =>{
-      console.log('result', result.data)
-      setCharacters(result.data)
-    })
-  },[characterName])
 
-  function handleCharacterRequest(name: string) {
-    console.log('charName', name)
-    setCharacterName(name);
+  function handleCharacterRequest() {
+    console.log('charName', characterName);
+    
+    axios.get("/characters", {params: {name: characterName}}).then(result =>{
+        setButtonToggle(true)
+        console.log('result',result.data)
+        setCharacters(result.data);
+    })
+    console.log('chars',characters)
+    setButtonToggle(false);
   }
 
   function handleOnChange(event:any) {
+    console.log(event.target.value)
     setCharacterName(event.target.value);
+    console.log(characterName)
   }
 
   return (
     <div>
         <div>
             <input type="search" name="character-lookup" id="character-search" onChange={handleOnChange} />
-            {characters.length > 1 ? characters.map((character, index) => {
-                return <a onClick={() => handleCharacterRequest(character.name)}><p key={index}>{character.name}</p></a>
-            }) : characters.length === 1 ? 
+            <button type="submit" onClick={handleCharacterRequest}>Search</button>
+            {characters.length > 1 ? 
+                <div>
+                    <p>Did you mean?</p>
+                    {characters.map((character, index) => {
+                    return <a onClick={handleCharacterRequest} key={index}><p>{character.name}</p></a>})}
+                </div>
+             : characters.length === 1 ? 
             <div>
                 <p>{characters[0].name}</p>
                 <p>{characters[0].height}</p>
+                <p>{characters[0].hair_color}</p>
+                <p>{characters[0].birth_year}</p>
+                <p>{characters[0].mass}</p>
+                <div>
+                    {characters[0].films.map(film => {
+                        console.log(film);
+                        return film.title;
+                    })}
+                </div>
+                <div>
+                    {characters[0].starships.map(starship => {
+                        console.log(starship);
+                        return starship.name;
+                    })}
+                </div>
             </div> 
-            : 'Sorry, no character matches your search criteria.'}
+            : buttonToggle && characters.length === 0 ? 'Sorry, no character matches your search criteria.': ''}
         </div>
     </div>
   )
--- server/routers/characters.js
+++ server/routers/characters.js
@@ -4,15 +4,43 @@ const axios = require('axios');
 
 router.get('/', async (req, res) => {
     try {
-        console.log(req.query.name);
         const params = new URLSearchParams({
           search: req.query.name
         })
-        console.log(`${process.env.BASE_URL}people/?${params}`);
-        await axios.get(`${process.env.BASE_URL}people/?${params}`).then(response => res.send(response.data.results));
-      } catch (error) {
+        
+        await axios.get(`${process.env.BASE_URL}people/?${params}`).then(response => {
+            const characterInfo = response.data.results;
+           
+            if (characterInfo.length === 1) {
+                getFilms(characterInfo[0].films).then(filmData => {
+                    getStarships(characterInfo[0].starships).then(starshipData => {
+                        res.send([Object.assign(characterInfo[0], {starships: starshipData, films: filmData})]);
+                    })
+                })
+            } else {
+                res.send(characterInfo)
+            }
+        
+        })
+    } catch (error) {
         console.log(error);
-      }
+    }
 })
+
+
+async function getFilms(films) {
+    const filmObjs = await Promise.all(films.map(film => {
+        return axios.get(film).then(results => film = results.data);
+    }))
+    return filmObjs;
+}
+
+async function getStarships(starships) {
+    const starshipObjs = await Promise.all(starships.map(starship => {
+        return axios.get(starship).then(results => starship = results.data);
+    }))
+    return starshipObjs;
+}
+
 
 module.exports = router;
