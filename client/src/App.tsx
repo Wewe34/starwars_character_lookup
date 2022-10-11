1ed12b3d26ef5848b6952de0f76d69ccfefacd80
Cassandra Terry <cassandraterry@Cassandras-MacBook-Pro.local>
2022-10-11 18:06:47 -0500

    added boostrap, mobile first styling complete

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,12 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Profile from './Components/Profile';
+import {Button} from 'react-bootstrap';
 
 function App() {
 
   return (
     <div className="App">
-        <p>Star Wars Character Lookup</p>
         <Profile />
     </div>
   )
--- client/src/Components/Profile.tsx
+++ client/src/Components/Profile.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import {Button, Card} from 'react-bootstrap';
 
-interface ICharacters {
+interface ICharacter {
     name: string, 
     height: string, 
     mass: string, 
@@ -58,8 +59,26 @@ interface IStarship {
     url: string
 }
 
+interface ISpecies {
+    name: string, 
+    classification: string, 
+    designation: string, 
+    average_height: string, 
+    skin_colors: string, 
+    hair_colors: string, 
+    eye_colors: string, 
+    average_lifespan: string, 
+    homeworld: null, 
+    language: string, 
+    people: string[],
+    films: IFilm[],
+    created: string, 
+    edited: string, 
+    url: string
+} 
+
 function Profile() {
-  const [characters, setCharacters] = useState<ICharacters[]>([]);
+  const [characters, setCharacters] = useState<ICharacter[]>([]);
   const [characterName, setCharacterName] = useState<String>('');
   const [buttonToggle, setButtonToggle] = useState<Boolean>(false);
   
@@ -84,9 +103,15 @@ function Profile() {
 
   return (
     <div>
-        <div>
-            <input type="search" name="character-lookup" id="character-search" onChange={handleOnChange} />
-            <button type="submit" onClick={handleCharacterRequest}>Search</button>
+        <img className="img-fluid w-50 py-4" src="/star-wars-logo.png" alt="text star wars" />
+        <p className="h1 pb-2">Character Lookup</p>
+        <div className='container'>
+            <div className="input-group mb-3">
+                <input type="search" className="form-control" placeholder="Character name..." aria-label="Character name" onChange={handleOnChange}/>
+                <div className="input-group-append">
+                    <Button className="btn-success" type="button" onClick={handleCharacterRequest}>Search</Button>
+                </div>
+            </div>
             {characters.length > 1 ? 
                 <div>
                     <p>Did you mean?</p>
@@ -95,23 +120,50 @@ function Profile() {
                 </div>
              : characters.length === 1 ? 
             <div>
-                <p>{characters[0].name}</p>
-                <p>{characters[0].height}</p>
-                <p>{characters[0].hair_color}</p>
-                <p>{characters[0].birth_year}</p>
-                <p>{characters[0].mass}</p>
-                <div>
+                <p className="display-4 text-uppercase text-danger">{characters[0].name}</p>
+                <p>{` ${characters[0].name} is of the species, ${characters[0].species} which speaks the language
+                    ${characters[0]}`}</p>
+                <div className="row p-2">
+                    <Card className="col m-1 bg-primary bg-gradient">
+                        <Card.Text className="text-white m-0 h1 pt-4">{characters[0].birth_year}</Card.Text>
+                        <Card.Text className="text-dark h4 pb-4">Birth Year</Card.Text>
+                    </Card>
+                    <Card className="col m-1 bg-primary bg-gradient">
+                        <Card.Text className="text-white m-0 h1 pt-4 text-uppercase">{characters[0].hair_color}</Card.Text>
+                        <Card.Text className="text-dark h4 pb-4">Hair Color</Card.Text>
+                    </Card>
+                </div>
+                <div className="row p-2">
+                    <Card className="col m-1 bg-primary bg-gradient">
+                        <Card.Text className="text-white m-0 h1 pt-4">{characters[0].height}</Card.Text>
+                        <Card.Text className="text-dark h4 pb-4">Height</Card.Text>
+                    </Card>
+                    <Card className="col m-1 bg-primary bg-gradient">
+                        <Card.Text className="text-white m-0 h1 pt-4">{characters[0].mass}</Card.Text>
+                        <Card.Text className="text-dark h4 pb-4">Weight</Card.Text>
+                    </Card>
+                </div>
+                <Card className="bg-light bg-gradient">
+                    <Card.Text className="text-dark h1 pt-4">FILMS</Card.Text>
                     {characters[0].films.map(film => {
                         console.log(film);
-                        return film.title;
+                        return <Card.Body className="bg-dark bg-gradient m-2 rounded">
+                                    <Card.Text className="text-warning m-0 h4">{film.title}</Card.Text>
+                                    <Card.Text className="text-warning m-0">Episode: {film.episode_id}</Card.Text>
+                                    <Card.Text className="text-warning m-0">Release Date: {film.release_date}</Card.Text>
+                                </Card.Body>
                     })}
-                </div>
-                <div>
-                    {characters[0].starships.map(starship => {
+                </Card>
+                <Card className="my-2 bg-light">
+                    <Card.Text className="text-dark h1 pt-4">STARSHIPS</Card.Text>
+                    {characters[0].starships.length > 0 ? characters[0].starships.map(starship => {
                         console.log(starship);
-                        return starship.name;
-                    })}
-                </div>
+                        return <Card.Body className="bg-dark bg-gradient m-2 rounded">
+                                    <Card.Text className="text-primary m-0 h4">{starship.name}</Card.Text>
+                                    <Card.Text className="text-primary m-0">Model: {starship.model}</Card.Text>
+                                </Card.Body>
+                    }): 'No starships.'}
+                </Card>
             </div> 
             : buttonToggle && characters.length === 0 ? 'Sorry, no character matches your search criteria.': ''}
         </div>
